e3f24268e4949b315e2ec64fadd1119ff20a6502
Tommy <rsslone1120@gmail.com>
2026-05-20 23:37:42 -0400

    Implement FML Update Check

--- src/main/java/com/refitbench/dysoncubeproject/DysonCubeProject.java
+++ src/main/java/com/refitbench/dysoncubeproject/DysonCubeProject.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, updateJSON = Reference.UPDATE_JSON)
 public class DysonCubeProject {
 
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_NAME);
